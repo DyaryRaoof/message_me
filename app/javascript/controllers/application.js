d974f053f759a6acb1f27503b4f02d8cdf81f628
Dyary Raoof <dyray2020@gmail. om>
2023-07-25 14:38:22 +0300

    remove useless semantic ui files

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,25 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import "jquery"; // this import first
-import "script"; // then your other imports that use `$`
-
-// NOTE: don't use relative imports: `import "./script"`
-//       add `pin "script"` to `importmap.rb`
-
-
-
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus = application
-
-
-console.log($); // ok
-
-$(document).on('turbolinks:load', function() {
-    $('.ui.dropdown').dropdown();
-    console.log("hello world")
-});
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/custom", under: "custom"
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
+
